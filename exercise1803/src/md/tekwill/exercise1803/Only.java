5dd6ef7da2d907f201176126d78c89a6d2e3ab8c
Serghei Chiriliuc <serghei.chiriliuc@inbox.ru>
2021-03-18 18:25:04 +0200

    new

---
+++ exercise1803/src/md/tekwill/exercise1803/Only.java
@@ -0,0 +1,17 @@
+package md.tekwill.exercise1803;
+
+public class Only {
+    public static void main(String[] args){
+        System.out.println("Ex1:Hope is a dangerous thing my friend, it can kill a man.");
+        System.out.println("Ex2:Hope is a dangerous thing my friend, it can kill a man.__The Shawshank redemption__Red__1994");
+        System.out.println("Ex3:\n" +
+
+                "   X                      X   \n"+
+                "   X                      X   \n"+
+                "   X      XXXXXXXXXX      X   \n"+
+                "   XXXXX  X        X  XXXXX   \n"+
+                "   X   X  X        X  X   X   \n"+
+                "   X   X  X        X  X   X      ");
+    }
+
+}
